93bd4b3f917a38cea5d2021758a62adce7095ace
bassam-radman <bassamradman0088@gmail.com>
2025-12-16 22:18:13 +0300

    Update EmailSender.cs

--- EmailSender.cs
+++ EmailSender.cs
@@ -1,1 +1,15 @@
+using Microsoft.AspNetCore.Identity.UI.Services;
 
+namespace WebHomework.Data // veya namespace WebHomework.Services
+{
+    // Bu sınıf sisteme "Ben e-posta göndericisiyim" der ama aslında hiçbir şey yapmaz.
+    // Böylece hata almadan kayıt işlemini geçeriz.
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // Gerçekten mail atma, sadece görevi tamamlandı olarak işaretle.
+            return Task.CompletedTask;
+        }
+    }
+}
